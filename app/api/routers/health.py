ddaffe599fadba5b0c42ae4464424f44cc730717
hatred <gajethatredddd@vk.com>
2025-11-29 21:09:38 +0500

    fix health

--- app/api/routers/health.py
+++ app/api/routers/health.py
@@ -1,46 +1,13 @@
-pipeline
-{
-    agent
-any
+from fastapi import APIRouter
 
-stages
-{
-    stage('Build')
-{
-    steps
-{
-    sh
-'docker-compose build'
-}
-}
+router = APIRouter()
 
-stage('Test')
-{
-    steps
-{
-    echo
-'Тесты пропущены, всё чётко! ✅'
-}
-}
+@router.get("/health",tags =["Здоровье"],summary = ["Проверить жив ли сервис"],include_in_schema=True)
+async def health_check():
+    return {"status": "active"}
 
-stage('Deploy')
-{
-    steps
-{
-    sh
-'docker-compose down'
-sh
-'docker-compose up -d'
-}
-}
-}
+@router.get("/test/division-by-zero",tags =["ОШИБКИ"],summary = ["Деление на ноль"])
+async def division_by_zero():
+    result = 1 / 0
+    return {"result": result}
 
-post
-{
-    always
-{
-    echo
-'Пайплайн завершен!'
-}
-}
-}
